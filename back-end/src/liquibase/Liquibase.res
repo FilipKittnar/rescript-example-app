30e349b3642d761d9313420269ee43352c0662b8
Filip Kittnar <filip.kittnar-renexter@renault.cz>
2020-10-03 20:38:25 +0200

    Fix database changelog

--- back-end/src/liquibase/Liquibase.res
+++ back-end/src/liquibase/Liquibase.res
@@ -1,7 +1,7 @@
 open Js.Promise
 
 let liquibase = ResLiquibase.liquibase({
-  changeLogFile: "liquibase/liquibase-changelog.sql",
+  changeLogFile: "src/liquibase/liquibase-changelog.sql",
   driver: "org.postgresql.Driver",
   classpath: "../database/postgresql-42.2.16.jar",
   url: "jdbc:postgresql://localhost:5432/postgres",
--- back-end/src/liquibase/liquibase-changelog.sql
+++ back-end/src/liquibase/liquibase-changelog.sql
@@ -1,33 +1,33 @@
 --liquibase formatted sql
 				
---changeset kittnarf:user-create-01
-create table user (  
+--changeset kittnarf:person-create-01
+create table person (  
     id uuid primary key,
     name varchar(255) not null,
     age smallint null,
     active boolean not null,
-    created timestamp not null with time zone default current_timestamp,
+    created timestamp with time zone default current_timestamp not null,
     last_modified timestamp null
 );  
---rollback drop table user; 
+--rollback drop table person; 
 
---changeset kittnarf:user-insert-01
-insert into user
+--changeset kittnarf:person-insert-01
+insert into person
 (id, name, age, active)
 values
 ('cfba3b8e-4fe2-4436-ac9b-0d1d507624e9', 'John Doe', 37, true);
---rollback delete from user where id = 'cfba3b8e-4fe2-4436-ac9b-0d1d507624e9'; 
+--rollback delete from person where id = 'cfba3b8e-4fe2-4436-ac9b-0d1d507624e9'; 
 
---changeset kittnarf:user-insert-02
-insert into user
+--changeset kittnarf:person-insert-02
+insert into person
 (id, name, age, active)
 values
 ('5274091f-7da6-4876-a947-19bf829a7825', 'Francis Blake', 24, false);
---rollback delete from user where id = '5274091f-7da6-4876-a947-19bf829a7825'; 
+--rollback delete from person where id = '5274091f-7da6-4876-a947-19bf829a7825'; 
 
---changeset kittnarf:user-insert-03
-insert into user
+--changeset kittnarf:person-insert-03
+insert into person
 (id, name, age, active)
 values
 ('acac2f61-d7b6-4e52-81c0-5c71663fbc99', 'Lucy Liu', null, true);
---rollback delete from user where id = 'acac2f61-d7b6-4e52-81c0-5c71663fbc99'; 
+--rollback delete from person where id = 'acac2f61-d7b6-4e52-81c0-5c71663fbc99'; 
